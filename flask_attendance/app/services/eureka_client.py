f77a6af0033ba401654dce93ad0155d3ef6c693c
Soufiane sejjari <sejjari.soufian@gmail.com>
2024-12-17 23:37:18 +0100

    Add environment variable support for Eureka server and configure logging

--- flask_attendance/app/services/eureka_client.py
+++ flask_attendance/app/services/eureka_client.py
@@ -1,9 +1,15 @@
 import py_eureka_client.eureka_client as eureka_client
-
+import os 
+import logging
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 def register_with_eureka(app_name: str, port: int) -> None:
     eureka_client.init(
-        eureka_server="http://localhost:8761/eureka",
+        eureka_server = os.getenv("EUREKA_SERVER", "http://localhost:8761/eureka"),
         app_name=app_name,
         instance_port=port,
         instance_host="localhost"
     )
+    logger.info("the eurikaservice url is {} ",os.getenv("EUREKA_SERVER", "http://localhost:8761/eureka") )
+    
--- flask_attendance/run.py
+++ flask_attendance/run.py
@@ -1,13 +1,16 @@
 from app import create_app
 from app.services.eureka_client import register_with_eureka
 import os
+import logging
 from dotenv import load_dotenv
-
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 # Load environment variables
 load_dotenv()
 # Create and run the app
 app = create_app()
 
 if __name__ == "__main__":
+    logger.info("the eurikaservice url is {} ",os.getenv("EUREKA_SERVER", "http://localhost:8761/eureka") )
     register_with_eureka("face-detection-service", 5000)
     app.run(host="0.0.0.0", port=5000)
