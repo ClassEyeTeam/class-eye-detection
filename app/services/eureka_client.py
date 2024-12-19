9dee74723942915d4042c00738c7d03ec23c4d58
Soufiane sejjari <sejjari.soufian@gmail.com>
2024-12-19 16:38:00 +0100

    Enhance Eureka client registration with dynamic instance IP and update Docker ports

--- app/services/eureka_client.py
+++ app/services/eureka_client.py
@@ -1,6 +1,7 @@
 import py_eureka_client.eureka_client as eureka_client
 import os
 import logging
+import socket
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -9,10 +10,18 @@ logger = logging.getLogger(__name__)
 def register_with_eureka(app_name: str, port: int) -> None:
     eureka_server = os.getenv("EUREKA_SERVER", "http://localhost:8761/eureka")
     logger.info("Eureka server URL: %s", eureka_server)
+
+    # Get the container's hostname or IP address
+    instance_host = socket.gethostname()
+    instance_ip = socket.gethostbyname(instance_host)
+
+    logger.info("Instance host: %s", instance_host)
+    logger.info("Instance IP: %s", instance_ip)
+
     eureka_client.init(
         eureka_server=eureka_server,
         app_name=app_name,
         instance_port=port,
-        instance_host="localhost"
+        instance_host=instance_ip
     )
     logger.info("Registered with Eureka: %s", eureka_server)
